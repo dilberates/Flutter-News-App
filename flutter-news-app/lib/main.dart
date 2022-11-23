20096cc8a98ecbbc7e33b17f79ffc03e9e505b8c
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-23 20:04:58 +0300

    homePage.dart create
    homePage oluşturuldu.

--- flutter-news-app/lib/main.dart
+++ flutter-news-app/lib/main.dart
@@ -3,3 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: HomePage(),
+    );
+  }
+}
+
