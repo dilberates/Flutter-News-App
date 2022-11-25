64c16c3158ccabce9127eb19d2b9ffc7103109a7
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-25 22:27:59 +0300

    Update articleViewModel.dart

--- flutter-news-app/lib/view-model/articleViewModel.dart
+++ flutter-news-app/lib/view-model/articleViewModel.dart
@@ -4,4 +4,13 @@ class ArticleViewModel{
   ArticleModel? articleModel;
 
   ArticleViewModel({this.articleModel});
+
+  get sourceName => articleModel?.sourceModel?.name;
+  get title => articleModel?.title;
+  get author => articleModel?.author;
+  get publishedAt => articleModel?.publishedAt;
+  get description => articleModel?.description;
+  get content => articleModel?.content;
+  get urlToImage => articleModel?.urlToImage;
+  get url => articleModel?.urlToImage;
 }
