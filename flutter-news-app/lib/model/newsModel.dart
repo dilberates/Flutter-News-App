63e144fecc7e46171ff7fade3d87b162dbc4e877
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-27 13:24:56 +0300

    Update newsModel.dart
    Yeni model eklendi.

--- flutter-news-app/lib/model/newsModel.dart
+++ flutter-news-app/lib/model/newsModel.dart
@@ -1,9 +1,40 @@
+import 'dart:convert';
+
 class NewsModel {
-  List<dynamic>? articles;
+  String? title;
+  String? description;
+  String? publishedAt;
+  String? urlToImage;
+  String? url;
+  String? source;
 
-  NewsModel({this.articles});
+  NewsModel(this.title,this.description,this.publishedAt,this.urlToImage,this.url,this.source);
 
-  factory NewsModel.fromMap(Map<String, dynamic> map){
-    return NewsModel(articles: map['articles']);
+  factory NewsModel.fromJson(Map<String, dynamic> map){
+    return NewsModel(
+      map['title'],
+      map['description'],
+      map['publishedAt'],
+      map['urlToImage'],
+      map['url'],
+      map['source'],
+    );
   }
+
+  static Map<String,dynamic> toMap(NewsModel news)=>{
+    'title': news.title,
+    'description': news.description,
+    'publishedAt': news.publishedAt,
+    'urlToImage': news.urlToImage,
+    'url': news.url,
+    'source': news.source,
+  };
+
+  static String encode(List<NewsModel> news) => json.encode(
+    news.map<Map<String, dynamic>>((news2) => NewsModel.toMap(news2)).toList(),
+  );
+
+
+  static List<NewsModel> decode(String news) =>
+      (json.decode(news) as List<dynamic>).map<NewsModel>((item) => NewsModel.fromJson(item)).toList();
 }
